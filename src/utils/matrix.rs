af302166c01cd5be28ccb8a4008e60aeac1469ef
SpekalsG3 <karpick.kirill@gmail.com>
2024-02-23 04:02:29 +0300

    implement getting inverse MDS matrix

--- src/utils/matrix.rs
+++ src/utils/matrix.rs
@@ -1,3 +1,4 @@
+use crate::field::field::Field;
 use crate::field::field_element::FieldElement;
 
 // source - https://rosettacode.org/wiki/Reduced_row_echelon_form
@@ -64,11 +65,53 @@ pub fn transpose<'m>(matrix: Vec<Vec<FieldElement<'m>>>) -> Vec<Vec<FieldElement
         .collect()
 }
 
+pub fn inverse<'m>(
+    field: &'m Field,
+    matrix: Vec<Vec<FieldElement<'m>>>,
+) -> Result<Vec<Vec<FieldElement<'m>>>, String> {
+    let n = matrix.len();
+    let mut matrix = matrix
+        .into_iter()
+        .enumerate()
+        .map(|(i, row)| {
+            if row.len() == n {
+                let mut v = vec![field.zero(); n];
+                v[i] = field.one();
+                Ok(row.into_iter().chain(v).collect())
+            } else {
+                Err("Inverse exists only for square matrices")
+            }
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+    rref(&mut matrix);
+
+    let (identity, matrix) = matrix
+        .into_iter()
+        .fold((Vec::with_capacity(n), Vec::with_capacity(n)), |mut acc, row| {
+            acc.0.push(row[..n].to_vec());
+            acc.1.push(row[n..].to_vec());
+            acc
+        });
+
+    let one = field.one();
+    for (i, row) in identity.iter().enumerate() {
+        if
+            row[..i].iter().position(|el| !el.is_zero()).is_some()
+            || &row[i] != &one
+            || row[(i+1)..].iter().position(|el| !el.is_zero()).is_some()
+        {
+            return Err("Couldnt construct identity matrix to find inverse".to_string())
+        }
+    }
+
+    Ok(matrix)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::field::field::{Field, FIELD_PRIME};
     use crate::field::field_element::FieldElement;
-    use crate::utils::matrix::{rref, transpose};
+    use crate::utils::matrix::{inverse, rref, transpose};
 
     #[test]
     fn test_transpose () {
@@ -104,5 +147,37 @@ mod tests {
             vec![FieldElement::new(&field, 0), FieldElement::new(&field, 0), FieldElement::new(&field, 1), -FieldElement::new(&field, 2)],
         ];
         assert_eq!(matrix, res);
+    }
+
+    #[test]
+    fn test_inverse () {
+        let field = Field::new(FIELD_PRIME);
+
+        {
+            let matrix = vec![
+                vec![FieldElement::new(&field, 1), FieldElement::new(&field, 2), FieldElement::new(&field, 3)],
+                vec![FieldElement::new(&field, 0), FieldElement::new(&field, 1), FieldElement::new(&field, 4)],
+                vec![FieldElement::new(&field, 5), FieldElement::new(&field, 6), FieldElement::new(&field, 0)],
+            ];
+            let matrix = inverse(&field, matrix);
+            assert!(matrix.is_ok());
+            assert_eq!(matrix.unwrap(), vec![
+                vec![-FieldElement::new(&field, 24), FieldElement::new(&field, 18), FieldElement::new(&field, 5)],
+                vec![FieldElement::new(&field, 20), -FieldElement::new(&field, 15), -FieldElement::new(&field, 4)],
+                vec![-FieldElement::new(&field, 5), FieldElement::new(&field, 4), FieldElement::new(&field, 1)],
+            ]);
+        }
+        {
+            let matrix = vec![
+                vec![FieldElement::new(&field, 270497897142230380135924736767050121214), FieldElement::new(&field,  4)],
+                vec![FieldElement::new(&field, 270497897142230380135924736767050121205), FieldElement::new(&field, 13)],
+            ];
+            let matrix = inverse(&field, matrix);
+            assert!(matrix.is_ok());
+            assert_eq!(matrix.unwrap(), vec![
+                vec![FieldElement::new(&field, 210387253332845851216830350818816760948), FieldElement::new(&field, 60110643809384528919094385948233360270)],
+                vec![FieldElement::new(&field, 90165965714076793378641578922350040407), FieldElement::new(&field, 180331931428153586757283157844700080811)],
+            ])
+        }
     }
 }
